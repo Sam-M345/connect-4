aeef4ffc9b417a7890a5dbddbfd6c7e7fac55651
Sam Mirazi <sasan345@gmail.com>
2025-05-19 21:33:10 -0700

    2 distinct backgrounds

--- localhost.py
+++ localhost.py
@@ -8,7 +8,8 @@ server = Server()
 # When these files change, the browser will automatically reload.
 # For CSS changes, it will inject the styles without a full page reload.
 server.watch('index.html')
-server.watch('style.css')
+server.watch('CSS/style.css')
+server.watch('CSS/ocean-theme.css')
 server.watch('animation.js')
 server.watch('script.js')
 server.watch('game/*.js') # Watches all .js files in the 'game' directory
